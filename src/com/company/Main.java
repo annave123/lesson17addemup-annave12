b21ac71e3103871728682d9617bbf2ec17162464
annave123 <tony.nave@aol.com>
2016-10-18 20:20:59 -1000

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,31 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner kb = new Scanner(System.in);
-        System.out.print("Enter something like 8 + 33 + 1,345 +137 : ");
-        String s = kb.nextLine( ); //Best to store in a String and then create a new Scanner
-        //object; otherwise, it can get stuck waiting for input.
-        Scanner sc = new Scanner(s);
-        //Set delimiters to a plus sign surrounded by any amount of white space...or...
-        // a minus sign surrounded by any amount of white space.
-        sc.useDelimiter("\\s*\\+\\s*");
-        int sum = 0;
-        while(sc.hasNextInt( ))
-            sum = sum + sc.nextInt( );
 
+        System.out.print("Enter something like 8 + 33 + 1345 - 137 : ");
+        String s = kb.nextLine();
+
+        Scanner sc = new Scanner(s);
+        int sum = 0;
+
+        if (s.contains("+")) {
+            sc.useDelimiter("\\s*\\+\\s*");
+            while (sc.hasNextInt()) {
+                sum = sum + sc.nextInt();
+            }
+        }
+
+        if (s.contains("-")) {
+            sc.useDelimiter("\\s*\\-\\s*");
+            while (sc.hasNextInt()) {
+                sum = sum - sc.nextInt() * 2;
+            }
+        }
         System.out.println("Sum is: " + sum);
     }
 }
